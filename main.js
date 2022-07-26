2879e80cae5617edb845ea32c6d489e1cc6df57d
Alexxcamargo1000 <alexxcamargo1000@gmail.com>
2022-07-26 12:35:20 -0300

    styles and variables

---
+++ main.js
@@ -0,0 +1,25 @@
+const btnOne = document.querySelect('#one');
+const btnZero = document.querySelect('#zero');
+const btnTwo = document.querySelect('#two');
+const btnThree = document.querySelect('#three');
+const btnFour = document.querySelect('#four');
+const btnFive = document.querySelect('#five');
+const btnSix = document.querySelect('#six');
+const btnSeven = document.querySelect('#seven');
+const btnEight = document.querySelect('#eight');
+const btnNine = document.querySelect('#nine');
+
+const btnMultiplication = document.querySelect('#multiplication');
+const btnPlus = document.querySelect('#plus');
+const btnMinus = document.querySelect('#minus');
+const btnClear = document.querySelect('#clear');
+const btnComma = document.querySelect('#comma');
+const btnRadix = document.querySelect('#radix');
+const btnCube = document.querySelect('#cube');
+const btnSquared = document.querySelect('#squared');
+const btnDivider = document.querySelect('#divider');
+
+
+
+
+
