e7308a3cc4e012b6de784452402111ca30d58cd2
Alex <alexxcamargo1000@gmail.com>
2022-07-27 15:49:50 -0300

    using eval

--- main.js
+++ main.js
@@ -1,25 +1,57 @@
-const btnOne = document.querySelect('#one');
-const btnZero = document.querySelect('#zero');
-const btnTwo = document.querySelect('#two');
-const btnThree = document.querySelect('#three');
-const btnFour = document.querySelect('#four');
-const btnFive = document.querySelect('#five');
-const btnSix = document.querySelect('#six');
-const btnSeven = document.querySelect('#seven');
-const btnEight = document.querySelect('#eight');
-const btnNine = document.querySelect('#nine');
+const buttonsNumbers = document.querySelectorAll(".number");
 
-const btnMultiplication = document.querySelect('#multiplication');
-const btnPlus = document.querySelect('#plus');
-const btnMinus = document.querySelect('#minus');
-const btnClear = document.querySelect('#clear');
-const btnComma = document.querySelect('#comma');
-const btnRadix = document.querySelect('#radix');
-const btnCube = document.querySelect('#cube');
-const btnSquared = document.querySelect('#squared');
-const btnDivider = document.querySelect('#divider');
+const btnMultiplication = document.querySelector("#multiplication");
+const btnPlus = document.querySelector("#plus");
+const btnMinus = document.querySelector("#minus");
+const btnClear = document.querySelector("#clear");
+const btnComma = document.querySelector("#comma");
+const btnRadix = document.querySelector("#radix");
+const btnCube = document.querySelector("#cube");
+const btnSquared = document.querySelector("#squared");
+const btnDivider = document.querySelector("#divider");
+const btnEqual = document.querySelector("#equal");
+
+const screen = document.getElementById("screen");
+
+let numberOnScreen = [];
+let controlNumbers = [];
+let result = 0;
+
+buttonsNumbers.forEach((btn) => {
+  btn.onclick = () => {
+    if (numberOnScreen.length < 18) {
+      numberOnScreen.push(Number(btn.textContent));
+      screen.value = numberOnScreen.join("");
+    }
+  };
+});
+
+btnPlus.addEventListener("click", () => {
+  controlNumbers = [...controlNumbers, Number(screen.value), '+'];
+  numberOnScreen = [];
+});
+
+btnMinus.addEventListener("click", () => {
+  controlNumbers = [...controlNumbers, Number(screen.value), '-'];
+  numberOnScreen = [];
+});
+
+btnEqual.addEventListener("click", () => {
+  controlNumbers = [...controlNumbers, Number(screen.value)];
+  console.log(controlNumbers);
+  result = eval(controlNumbers.join(""))
+  screen.value = result;
+
+  controlNumbers = [];
+  result = 0;
+});
+
+btnClear.addEventListener("click", clear);
 
 
-
-
-
+function clear() {
+  controlNumbers = [];
+  result = 0;
+  numberOnScreen = [];
+  screen.value = 0;
+}
